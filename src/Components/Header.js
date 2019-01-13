13126c81f81823784cb7222da4cfe3758a8a1855
LoriDeCoder <ldavis1005@gmail.com>
2019-01-13 00:37:28 -0600

    added logo icon to nav bar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,12 @@ import React from 'react';
 const Header = () => (
     <div className="navheader">
         <nav className="nav navbar-light">
-            <a className="navbar-brand" href="/">Team Breakthrough</a>
+            <a className="navbar-brand" href="/"> <img className="breakthroughGreyTeal"
+                src={require("../Images/brLOGOsmall.png")} height="100" width="100" />
+                </a>
+
+
+
             <ul className="nav navbar-nav navbar-right">
                 <li><a href="/Anders">Anders</a></li>
                 <li><a href="/Lori">Lori</a></li>
