18c43ce5a426e60a0882be031e48a87ed018dced
Anders <anderskornkven@gmail.com>
2019-01-15 17:36:08 -0700

    fixed the nav finally :D

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,21 +1,38 @@
 import React from 'react';
 
 const Header = () => (
-    <div className="navheader">
-        <nav className="nav navbar-light">
+    <div>
+    {/* <nav className="navbar navbar-light">
+        <div className="container-fluid">
+            <div className="navbar-header">
             <a className="navbar-brand" href="/"> <img className="breakthroughGreyTeal"
                 src={require("../Images/brLOGOsmall.png")} height="100" width="100" />
                 </a>
-
-
-
             <ul className="nav navbar-nav navbar-right">
-                <li><a id="Anders1" href="/Anders">Anders</a></li>
-                <li><a id="Lori1" href="/Lori">Lori</a></li>
-                <li><a id="Contact1" href="/Developer">Contact Us</a></li>
+                <li><a className="item1" href="/Anders">Anders</a></li>
+                <li><a className="item1" href="/Lori">Lori</a></li>
+                <li><a className="item1" href="/Developer">Contact Us</a></li>
             </ul>
-        </nav>
-    </div>    
+            </div>
+            </div>
+    </nav> */}
+
+
+    <nav className="navbar navbar-light">
+        <div className="container-fluid">
+        <div className="navbar-header">
+            <a className="navbar-brand" href="/"> <img className="breakthroughGreyTeal"
+            src={require("../Images/brLOGOsmall.png")} height="100" width="100" />
+            </a>
+        </div>
+        <ul className="nav navbar-nav navbar-right item2">
+            <li><a className="item1" href="/Anders">Anders</a></li>
+            <li><a className="item1" href="/Lori">Lori</a></li>
+            <li><a className="item1" href="/Developer">Contact Us</a></li>
+        </ul>
+        </div>
+    </nav>
+    </div>        
 );
 
 export default Header;
--- src/Screens/Developer.js
+++ src/Screens/Developer.js
@@ -15,13 +15,34 @@ class Developer extends Component {
     
     render() {
         return (
-    
-    <div>
-        <Header/>
-        <h2 className="Title"> Please leave your name and information</h2>
-        <Form onSubmit={fields => this.onSubmit(fields)}/>
-        <div className="ContactBackground"></div>
-    </div>
+        <div className="ContactBackground">
+            <Header/>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <h2 className="Title"> Please leave your name and information</h2>
+            <div className="container">
+                <div className="row item3">
+                    <div className="col-sm-0 item3">
+                
+                    </div>
+                    <div className="col-sm-12 item3">
+                    <Form onSubmit={fields => this.onSubmit(fields)}/>
+                    </div>
+                    <div className="col-sm-0 item3">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
         );
     }
 }
