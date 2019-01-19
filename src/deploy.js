130f8441ea15832dd9c21caa6baf08ef9ad54577
Anders <anderskornkven@gmail.com>
2019-01-18 19:11:06 -0700

    Anders' page 99% done

--- src/deploy.js
+++ src/deploy.js
@@ -87,7 +87,7 @@ function sshConnect() {
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
       host: '54.174.39.207',
       username: 'ubuntu',
-      privateKey: 'hs-key.pem'
+      privateKey: 'TEAMbreakthrough.pem'
     })
     .then(function() {
       console.log('SSH Connection established.');
