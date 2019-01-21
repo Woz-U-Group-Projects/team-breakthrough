e553c102ea9812aad1de35332ad207f39a0375fc
LoriDeCoder <ldavis1005@gmail.com>
2019-01-21 13:52:52 -0600

    home page responsive

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -8,9 +8,9 @@ const Home = () => (
   <div className ="Home">
     <div className="HomeImage">
     <Header/>
-    <img className="brLogo" src={require('../Images/brLogo.png')}  />
- 
-        <div className="box">
+   
+        <div className="brBox">
+        <img className="brLogo" src={require('../Images/brLogo.png')}  />
             <div className="box1"></div>
             <div className="box2"></div>
             <div className="box3"></div>
