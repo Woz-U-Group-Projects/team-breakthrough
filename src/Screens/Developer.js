8c164ed96cf06d63f73c5908cb1e13fbbb35bb10
LoriDeCoder <ldavis1005@gmail.com>
2019-04-12 17:25:11 -0500

    final edits

--- src/Screens/Developer.js
+++ src/Screens/Developer.js
@@ -1,32 +1,29 @@
-import React, { Component } from 'react';
-import Header from '../Components/Header';
-import Form from '../Components/Form';
-import DeveloperStyles from './DeveloperStyles.css';
+import React, { Component } from "react";
+import Header from "../Components/Header";
+import Form from "../Components/Form";
+import DeveloperStyles from "./DeveloperStyles.css";
 
 class Developer extends Component {
-    state = {
-        fields: {}
-    };
-    
-    onSubmit = fields => {
-        this.setState({fields});
-    };
-    
-    render() {
-        return (
-<div className="backgroundContainer">
-            <Header/>     
-            <h2 className="thanks">Thanks for stopping by!</h2>
-            {/* col-md-offset-8 */}
-  <div className="col-md-offset-4 col-md-4 col-md-offset-4">
+  state = {
+    fields: {}
+  };
 
-    <Form onSubmit={fields => this.onSubmit(fields)} />
-  
-  </div>
-</div>
-        );
-    }
+  onSubmit = fields => {
+    this.setState({ fields });
+  };
+
+  render() {
+    return (
+      <div className="backgroundContainer">
+        <Header />
+        <h2 className="thanks">Thanks for stopping by!</h2>
+        {/* col-md-offset-8 */}
+        <div className="col-md-offset-4 col-md-4 col-md-offset-4">
+          <Form onSubmit={fields => this.onSubmit(fields)} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Developer;
-
